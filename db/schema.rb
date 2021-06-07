a88df1dcb8fb7a587f2de2f16dc54d39bb173407
Michel Oliveira <micheltdoliveira@gmail.com>
2021-06-07 01:27:00 -0300

    criando scaffold de usuario

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_06_07_042159) do
+
+  create_table "usuarios", force: :cascade do |t|
+    t.string "nome"
+    t.string "cpf"
+    t.string "funcao"
+    t.string "senha"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
