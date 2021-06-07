d53b6d6da33ab6dac03330cbdb5c436b3152ef68
Michel Oliveira <micheltdoliveira@gmail.com>
2021-06-07 14:24:41 -0300

    Adicionando validações de model para Endereco

--- app/models/endereco.rb
+++ app/models/endereco.rb
@@ -1,2 +1,7 @@
 class Endereco < ApplicationRecord
+
+    validates :cidade,presence: true, length:{minimum:3}
+    validates :bairro,presence: true, length:{minimum:3}
+    validates :logradouro,presence: true, length:{minimum:3}
+    validates :complemento,presence: true
 end
