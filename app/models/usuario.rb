f1bf0ebc8523740fd0543b8acde444b031e37c9e
Michel Oliveira <micheltdoliveira@gmail.com>
2021-06-08 13:36:42 -0300

    Adicionando login de Usuario

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,5 +1,7 @@
 class Usuario < ApplicationRecord
 
+    has_secure_password
+    
     validates :nome, presence: true, length: {minimum:10}, numericality: false
     validates :cpf,  presence: true, uniqueness: true, length: {is: 11}, numericality: {only_integer: true}
     validates :funcao, presence: true, length: {minimum: 5}
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   resources :servicos
   resources :ajudantes
   resources :usuarios
+  get 'entrar', to: 'sessions#new'
+  post 'entrar', to: 'session#create'
   
   root "sessions#new"
   #get 'sessions/new'
