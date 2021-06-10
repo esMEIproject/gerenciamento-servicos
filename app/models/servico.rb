e904532f8465fdcea60bb3351ede613e48e778f4
Gabriel Felix <gabibielmft@gmail.com>
2021-06-10 01:35:50 -0300

    adicionando validação de serviços

--- app/models/servico.rb
+++ app/models/servico.rb
@@ -1,4 +1,10 @@
 class Servico < ApplicationRecord
     belongs_to :usuario
     belongs_to :cliente
+
+    validates :tipoDoServico, presence: true, length: {minimum:8}
+    validates :descricao, presence: true, length: {minimum:8}
+    validates_date :data, on_or_after: lambda { Date.current }
+    validates :valorDoServico, presence: true, length: {minimum:1}
+
 end
