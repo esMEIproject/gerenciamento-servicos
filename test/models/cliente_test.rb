2fb2ff89a59cd011e571ca14bd74d5af416a6227
Emily <emilyalmeeidas@gmail.com>
2021-06-11 17:54:03 -0300

    Inserindo testes de model para cliente, material e usuario

--- test/models/cliente_test.rb
+++ test/models/cliente_test.rb
@@ -4,4 +4,43 @@ class ClienteTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "deve salvar Cliente cadastrado corretamente" do
+    usuario = Usuario.new nome: 'Usuario Teste',
+      cpf: '12345678912',
+      funcao: 'Confeiteiro',
+      password: 'password1'
+
+      usuario.save
+
+    cliente = Cliente.new nome: 'Cliente Teste 1',
+      cpf: '11111111111',
+      contato: '45214521',
+      endereco_attributes:{ cidade: 'Garanhuns',
+        bairro: 'Boa Vista',
+        logradouro: 'Rua Doze, numero 15',
+        complemento: 'Apartamento 112'}
+
+    assert cliente.save
+  end
+
+  test "nao deve salvar Cliente cadastrado com campo contato contendo numeros" do
+    usuario = Usuario.new nome: 'Usuario Teste',
+      cpf: '12345678912',
+      funcao: 'Confeiteiro',
+      password: 'password1'
+
+      usuario.save
+
+    cliente = Cliente.new nome: 'Cliente Teste 1',
+      cpf: '11111111111',
+      contato: '4521abcd',
+      endereco_attributes:{ cidade: 'Garanhuns',
+        bairro: 'Boa Vista',
+        logradouro: 'Rua Doze, numero 15',
+        complemento: 'Apartamento 112'}
+            
+
+    assert_not cliente.save
+  end
 end
