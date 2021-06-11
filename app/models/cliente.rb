3e37b88fdf4e0c408cbee7d998a4f5898b48a2fe
Emily <emilyalmeeidas@gmail.com>
2021-06-11 19:21:33 -0300

    Inserindo dependencia de destroy para os model

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -1,6 +1,6 @@
 class Cliente < ApplicationRecord
     has_one :endereco, :dependent => :destroy
-    has_many :servicos
+    has_many :servicos, :dependent => :restrict_with_error
     belongs_to :usuario
 
     validates :nome, presence: true, length: {minimum:8}, numericality: false
