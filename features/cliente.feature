359caf166f6285e431e0391bbc5a85c1feb301fc
Michel Oliveira <micheltdoliveira@gmail.com>
2021-06-12 16:24:34 -0300

    Adicionando teste cucumber para o Scenario: Cadastrar novo cliente

---
+++ features/cliente.feature
@@ -0,0 +1,14 @@
+Feature: Cliente
+  As a usuario do sistema de gerenciamento de servicos para MEI
+  I want to adicionar, visualizar, atualizar e remover um Cliente
+  So that eu nao tenha que fazer isso manualmente
+
+Scenario: Cadastrar novo cliente
+  Given existe um usuario com o nome 'usuario001', CPF-CNPJ '12345678999', funcao 'Gesseiro' e senha 'password'
+  And estou logado no sistema com o login '12345678999' e senha 'password'
+  When eu entro na pagina clientes
+  And clico em novo cliente
+  When eu preencho os campos com nome 'Fulano da Silva', CPF '00000000000', contato '952136988', cidade 'Garanhuns', bairro 'Boa Vista', logradouro 'Rua da Doze' e complemento 'Numero 123'
+  And eu clico em cadastrar cliente
+  Then eu vejo uma mensagem informando que 'Cliente foi criado com sucesso'
+  
---
+++ features/step_definitions/cliente_steps.rb
@@ -0,0 +1,38 @@
+Given('existe um usuario com o nome {string}, CPF-CNPJ {string}, funcao {string} e senha {string}') do |nome, cpf, funcao, senha|
+    visit '/entrar'
+    click_on 'Criar Usuário'
+    fill_in 'usuario_nome',	with: nome
+    fill_in 'usuario_cpf',	    with: cpf
+    fill_in 'usuario_funcao',	with: funcao
+    fill_in 'usuario_password',	with: senha
+    click_on 'submit'
+end
+
+Given('estou logado no sistema com o login {string} e senha {string}') do |cpf, senha|
+    fill_in 'session_cpf',	    with: cpf
+    fill_in 'session_password',	with: senha
+    click_on 'submit'
+end
+
+When('eu entro na pagina clientes') do
+    click_on 'clientes'
+end
+
+And('clico em novo cliente') do
+    click_on 'novo_cliente'
+end
+
+When('eu preencho os campos com nome {string}, CPF {string}, contato {string}, cidade {string}, bairro {string}, logradouro {string} e complemento {string}') do |nome, cpf, contato, cidade, bairro, logradouro, complemento|
+    fill_in 'cliente_nome',	with: nome
+    fill_in 'cliente_cpf',	with: cpf
+    fill_in 'cliente_contato',	with: contato
+    fill_in 'cliente_endereco_attributes_cidade',	with: cidade
+    fill_in 'cliente_endereco_attributes_bairro',	with: bairro
+    fill_in 'cliente_endereco_attributes_logradouro',	with: logradouro
+    fill_in 'cliente_endereco_attributes_complemento',	with: complemento
+end
+
+And ('eu clico em cadastrar cliente') do
+    click_on 'submit'
+end
+   
