bcf566844d7d38c6b401e25feda7d3f693328c7f
Bielfla27 <gabibielmft@outlook.com>
2021-06-20 15:49:00 -0300

    arrumando as validações de model material

--- app/models/material.rb
+++ app/models/material.rb
@@ -2,7 +2,7 @@ class Material < ApplicationRecord
     has_many :servico, :dependent => :nullify
     belongs_to :usuario
 
-    validates :nome, presence: true, length: {minimum:4}, numericality: false
-    validates :descricao, presence: true, length: {minimum:8}, numericality: false
+    validates :nome, presence: true, length: {in:4..35}, numericality: false
+    validates :descricao, presence: true, length: {in:8..50}, numericality: false
     validates :valor, presence: true, numericality: {only_float:true, greater_than: 10}    
 end
