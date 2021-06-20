a79e65c8cef8d9c084eb01d0979779c9417bf740
Emily <emilyalmeeidas@gmail.com>
2021-06-20 13:59:31 -0300

    Inserindo +3 testes de model para ajudante e servico

--- test/models/ajudante_test.rb
+++ test/models/ajudante_test.rb
@@ -6,15 +6,15 @@ class AjudanteTest < ActiveSupport::TestCase
   # end
   test "deve salvar Ajudante cadastrado corretamente" do
     usuario = Usuario.new nome: 'Usuario Teste',
-    cpf: '12345678912',
+    cpf: '12558002082',
     funcao: 'Gesseiro',
     password: 'password1'
 
     usuario.save
 
     ajudante = Ajudante.new nome: 'Ajudante Teste',
-    cpf: '12678721442',
-    contato: '00000000',
+    cpf: '33940570001',
+    contato: '12345678',
     precoMaoDeObra: '400',
     usuario_id: usuario.id
 
@@ -23,35 +23,96 @@ class AjudanteTest < ActiveSupport::TestCase
 
   test "Nao deve salvar Ajudante com cpf invalido" do
     usuario = Usuario.new nome: 'Usuario Teste',
-    cpf: '12345678912',
+    cpf: '12558002082',
     funcao: 'Gesseiro',
     password: 'password1'
 
     usuario.save
 
     ajudante = Ajudante.new nome: 'Ajudante Teste',
-    cpf: '12678721a',
-    contato: '00000000',
+    cpf: '3394057000a',
+    contato: '12345678',
     precoMaoDeObra: '400',
     usuario_id: usuario.id
 
     assert_not ajudante.save
   end
   
-  test "Nao deve salvar Ajudante contato invalido" do
+  test "Nao deve salvar Ajudante com contato invalido" do
     usuario = Usuario.new nome: 'Usuario Teste',
-    cpf: '12345678912',
+    cpf: '12558002082',
     funcao: 'Gesseiro',
     password: 'password1'
 
     usuario.save
 
     ajudante = Ajudante.new nome: 'Ajudante Teste',
-    cpf: '12678721a',
+    cpf: '33940570001',
     contato: 'AA000000',
     precoMaoDeObra: '400',
     usuario_id: usuario.id
 
     assert_not ajudante.save
+  end
+
+  #-------------------3 iteracao------------------------------
+
+  test "Nao deve salvar ajudante com campo nome vazio" do
+    usuario = Usuario.new nome: 'Usuario Teste',
+    cpf: '12558002082',
+    funcao: 'Gesseiro',
+    password: 'password1'
+
+    usuario.save
+
+    ajudante = Ajudante.new nome: '',
+    cpf: '33940570001',
+    contato: '12345678',
+    precoMaoDeObra: '400',
+    usuario_id: usuario.id
+
+    assert_not ajudante.save
+  end
+
+  test "Nao deve salvar ajudante com preco da mao de obra vazio" do
+    usuario = Usuario.new nome: 'Usuario Teste',
+    cpf: '12558002082',
+    funcao: 'Gesseiro',
+    password: 'password1'
+
+    usuario.save
+
+    ajudante = Ajudante.new nome: 'Ajudante Teste',
+    cpf: '33940570001',
+    contato: '12345678',
+    precoMaoDeObra: '',
+    usuario_id: usuario.id
+
+    assert_not ajudante.save
+  end
+
+  test "Nao deve salvar ajudante com cpf ja existente" do
+    usuario = Usuario.new nome: 'Usuario Teste',
+    cpf: '12558002082',
+    funcao: 'Gesseiro',
+    password: 'password1'
+
+    usuario.save
+
+    ajudante = Ajudante.new nome: 'Ajudante Teste',
+    cpf: '33940570001',
+    contato: '12345678',
+    precoMaoDeObra: '400',
+    usuario_id: usuario.id
+
+    ajudante.save
+
+    ajudante2 = Ajudante.new nome: 'Ajudante II',
+    cpf: '33940570001',
+    contato: '147852369',
+    precoMaoDeObra: '200',
+    usuario_id: usuario.id
+
+    assert_not ajudante2.save
   end
 end
