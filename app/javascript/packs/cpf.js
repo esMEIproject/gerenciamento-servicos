993c86af9a4eb0eb6dcd55935f97f3b07d6269a2
Michel Oliveira <micheltdoliveira@gmail.com>
2021-06-20 22:57:25 -0300

    Corrigindo problemas apontados pelo Codacy

--- app/javascript/packs/cpf.js
+++ app/javascript/packs/cpf.js
@@ -1,6 +1,5 @@
 const cpf = document.querySelector("#cpf");
 cpf.addEventListener("keyup", () => {
-  let value = cpf.value.replace(/[^a-z0-9]/gi, "").replace(/^([\d]{3})([\d]{3})?([\d]{3})?([\d]{2})?/, "$1.$2.$3-$4");
-  
+  let value = cpf.value.replace(/([\d]{3})([\d]{3})([\d]{3})([\d]{2})/, "$1.$2.$3-$4");
   cpf.value = value;
 });
