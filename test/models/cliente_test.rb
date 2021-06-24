8a7422352bd68c6bdff5cc6b27ed6ab8bbe85ba1
Michel Oliveira <micheltdoliveira@gmail.com>
2021-06-24 01:49:57 -0300

    Adicionando testes de model para cliente

--- test/models/cliente_test.rb
+++ test/models/cliente_test.rb
@@ -64,4 +64,57 @@ class ClienteTest < ActiveSupport::TestCase
 
     assert_not cliente.save
   end
+  test "nao deve salvar Cliente cadastrado com campo cpf inválido" do
+    usuario = Usuario.new nome: 'Usuario Teste',
+      cpf: '39775387485',
+      funcao: 'Confeiteiro',
+      password: 'password1'
+
+      usuario.save
+
+    cliente = Cliente.new nome: 'Cliente Teste 1',
+      cpf: '12345678900',
+      contato: '45214521',
+      endereco_attributes:{ cidade: 'Garanhuns',
+        bairro: 'Boa Vista',
+        logradouro: 'Rua Doze, numero 15',
+        complemento: 'Apartamento 112'},
+      usuario_id: usuario.id
+
+    assert_not cliente.save
+  end
+  test "nao deve salvar Cliente cadastrado sem nome" do
+    usuario = Usuario.new nome: 'Usuario Teste',
+      cpf: '39775387485',
+      funcao: 'Confeiteiro',
+      password: 'password1'
+
+      usuario.save
+
+    cliente = Cliente.new nome: '',
+      cpf: '37287511460',
+      contato: '45214521',
+      endereco_attributes:{ cidade: 'Garanhuns',
+        bairro: 'Boa Vista',
+        logradouro: 'Rua Doze, numero 15',
+        complemento: 'Apartamento 112'},
+      usuario_id: usuario.id
+
+    assert_not cliente.save
+  end
+  test "nao deve salvar Cliente cadastrado sem endereco" do
+    usuario = Usuario.new nome: 'Usuario Teste',
+      cpf: '39775387485',
+      funcao: 'Confeiteiro',
+      password: 'password1'
+
+      usuario.save
+
+    cliente = Cliente.new nome: '',
+      cpf: '37287511460',
+      contato: '45214521',
+      usuario_id: usuario.id
+
+    assert_not cliente.save
+  end
 end
