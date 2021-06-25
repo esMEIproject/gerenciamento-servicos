215fbf9a175ae0b5ddcb9c2021d70a8469b9cc05
Emily <emilyalmeeidas@gmail.com>
2021-06-25 00:09:38 -0300

    Alterando validações de model

--- app/models/endereco.rb
+++ app/models/endereco.rb
@@ -1,9 +1,9 @@
 class Endereco < ApplicationRecord
 
-    validates :cidade,presence: true,  length: {in:5..35}
+    validates :cidade,presence: true,  length: {in:3..35}
     validates :bairro,presence: true,  length: {in:4..35}
-    validates :logradouro,presence: true,  length: {in:5..35}
-    validates :complemento,presence: true,  length: {in:5..35}
+    validates :logradouro,presence: true,  length: {in:4..35}
+    validates :complemento,presence: true,  length: {in:3..35}
 
     belongs_to :cliente
 end
